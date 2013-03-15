ba621a93a02335c99c6f2d81118e13b4329d653e
Jose Lobato <jose.lobato@me.com>
2013-03-16 00:12:38 +0100

    Arrangement of the subview on sheet

--- JALExtendedActionSheet/JALExtendedActionSheet.m
+++ JALExtendedActionSheet/JALExtendedActionSheet.m
@@ -19,9 +19,10 @@
 
 @implementation JALExtendedActionSheet
 
-static const CGFloat kApearanceAnimationDuration = 0.3;
+static const CGFloat kApearanceAnimationDuration = 0.4;
 static const CGFloat kActionSheetHeight = 250.0;
 static const CGFloat kBackgroundAlpha = 0.7;
+static const CGFloat kButtonsHeight = 27.0;
 
 
 - (void)viewDidLoad
@@ -52,10 +53,78 @@ static const CGFloat kBackgroundAlpha = 0.7;
 	self.sheetVerticalConstraint = [NSLayoutConstraint constraintWithItem:sheet attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0];
 
 	[self.view addConstraint:self.sheetVerticalConstraint];
-	[self.view layoutIfNeeded];
 
+
+	// Message Label
+
+	UILabel *msgLabel = [self messageLabel];
+	[sheet addSubview:msgLabel];
+	views = NSDictionaryOfVariableBindings(msgLabel);
+	constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"|-20-[msgLabel]-20-|"
+														  options:0
+														  metrics:nil
+															views:views];
+	[sheet addConstraints:constraints];
+
+
+	// Buttons container (Scroll)
+
+	UIScrollView *buttonsContainer = [[UIScrollView alloc] init];
+	buttonsContainer.layer.borderColor = [UIColor whiteColor].CGColor;
+	buttonsContainer.layer.borderWidth = 1.0;
+	[sheet addSubview:buttonsContainer];
+	[buttonsContainer setTranslatesAutoresizingMaskIntoConstraints:NO];
+	views = NSDictionaryOfVariableBindings(buttonsContainer);
+	constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"|[buttonsContainer]|"
+														  options:0
+														  metrics:nil
+															views:views];
+	[sheet addConstraints:constraints];
+
+
+	// Pager under the Scroll
+
+	UIPageControl *pageControl = [[UIPageControl alloc] init];
+	pageControl.layer.borderColor = [UIColor whiteColor].CGColor;
+	pageControl.layer.borderWidth = 1.0;
+	[sheet addSubview:pageControl];
+	[pageControl setTranslatesAutoresizingMaskIntoConstraints:NO];
+	views = NSDictionaryOfVariableBindings(pageControl);
+	constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"|[pageControl]|"
+														  options:0
+														  metrics:nil
+															views:views];
+	[sheet addConstraints:constraints];
+
+
+	// Cancel Button
+
+	UIButton *cancelButton = [self cancelButton];
+	[sheet addSubview:cancelButton];
+	views = NSDictionaryOfVariableBindings(cancelButton);
+	constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"|-20-[cancelButton]-20-|"
+														  options:0
+														  metrics:nil
+															views:views];
+	[sheet addConstraints:constraints];
+
+
+	// Vertical arrangement
+
+	views = NSDictionaryOfVariableBindings(msgLabel, buttonsContainer, pageControl, cancelButton);
+	NSString *constraintStr = [NSString stringWithFormat:@"V:|-5-[msgLabel(16)][buttonsContainer][pageControl(10)]-7-[cancelButton(%f)]-7-|", kButtonsHeight];
+	constraints = [NSLayoutConstraint constraintsWithVisualFormat:constraintStr
+														  options:0
+														  metrics:nil
+															views:views];
+	[sheet addConstraints:constraints];
+
+
+	[sheet layoutIfNeeded];
 	self.sheetV = sheet;
+
 }
+
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -65,6 +134,7 @@ static const CGFloat kBackgroundAlpha = 0.7;
 		self.view.alpha = kBackgroundAlpha;
 	} completion:NULL];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -92,5 +162,30 @@ static const CGFloat kBackgroundAlpha = 0.7;
 															views:views];
 	[hostview.window addConstraints:constraints];
 }
+
+
+#pragma mark - basic Views customization
+
+- (UIButton *)cancelButton
+{
+	UIButton *newCancelButton = [UIButton buttonWithType:UIButtonTypeCustom];
+	[newCancelButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+	//	[newCancelButton setBackgroundColor:[UIColor redColor]];
+	newCancelButton.layer.backgroundColor = [UIColor redColor].CGColor;
+	newCancelButton.layer.cornerRadius = 5.0;
+	[newCancelButton setTitle:NSLocalizedString(@"Cancel",@"CancelButton on action sheet") forState:UIControlStateNormal];
+
+	return newCancelButton;
+}
+
+- (UILabel *)messageLabel
+{
+	UILabel *newLabel = [[UILabel alloc] init];
+	[newLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+	newLabel.textColor = [UIColor whiteColor];
+	//	newLabel.textAlignment = UITextAlignmentCenter;
+	return newLabel;
+}
+
 
 @end
